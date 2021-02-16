8fbf55cbf04e0a0150b760fc6dcdf51386e97e25
corey-marchand <corey.marchand@me.com>
2021-02-15 20:27:59 -0800

    testing out bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import { Button } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Puget Sound Notary</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="home">Home</Nav.Link>
+        <Nav.Link href="features">Features</Nav.Link>
+        <Nav.Link href="pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+    </>
   );
 }
 
